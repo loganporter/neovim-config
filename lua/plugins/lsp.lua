0230edf120d09082480d6a66d0f3e70a8d24be98
Logan <logan-porter@outlook.com>
2025-08-26 21:16:56 +1200

    add rust lsp support

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -57,7 +57,8 @@ return {
       ensure_installed = {
         "ts_ls",
         "lua_ls",
-        "ruff"
+        "ruff",
+        "rust_analyzer"
       },
       handlers = {
         function(server_name)
