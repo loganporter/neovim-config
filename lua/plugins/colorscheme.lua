f7c00ed09087743be4df2a8428d4139dea158efe
Logan <logan-porter@outlook.com>
2025-08-27 21:03:17 +1200

    change hover colour to black for higher contrast

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,13 @@
 return {
   "Mofiqul/vscode.nvim",
-  priority = 1000, -- Ensure this colorscheme is loaded first
-  lazy = false, -- Load this colorscheme immediately
+  priority = 1000,           -- Ensure this colorscheme is loaded first
+  lazy = false,              -- Load this colorscheme immediately
   opts = {
     terminal_colors = false, -- Disable terminal colors
+    group_overrides = {
+      -- Override specific highlight groups
+      NormalFloat = { bg = "#000000" },
+      Pmenu = { bg = "#000000" },
+    },
   }
 }
