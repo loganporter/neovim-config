774e52045eb35537ba92dfef83845e09da56c36d
Logan <logan-porter@outlook.com>
2025-09-10 21:23:51 +1200

    fix comment colours and use gitsigns plugin for blame and remove uneeded blame plugin

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -8,8 +8,6 @@ return {
       -- Override specific highlight groups
       NormalFloat = { bg = "#000000" },
       Pmenu = { bg = "#000000" },
-      -- Override here is for git blame plugin font colour and style
-      Comment = { fg = "#777777", italic = true },
       -- Override here is for git graph
       DiffDelete = { fg = "#444444", bg = "NONE" },
       DiffRemoved = { fg = "#444444", bg = "NONE" },
