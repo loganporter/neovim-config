be1522506cc2cf12047d774ab189c21ce49bcde0
Logan <logan-porter@outlook.com>
2026-02-09 22:16:57 +1300

    improve codecompanion rendering

--- lua/plugins/markview.lua
+++ lua/plugins/markview.lua
@@ -1,7 +1,18 @@
 return {
   'OXY2DEV/markview.nvim',
-  ft = 'markdown',
+  ft = { 'markdown', 'codecompanion' },
+  lazy = false,
   config = function()
-    require('markview').setup()
+    require('markview').setup({
+      preview = {
+        enable = true,
+        filetypes = { 'markdown', 'codecompanion' },
+        condition = function(buffer)
+          if vim.bo[buffer].filetype == 'codecompanion' then
+            return true
+          end
+        end,
+      },
+    })
   end,
 }
