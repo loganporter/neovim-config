9d8e009ba09c3220996c9beae230c61fea2076ba
Logan <logan-porter@outlook.com>
2026-02-13 19:51:28 +1300

    pin treesitter to master and update tab title based on working directory

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,3 +2,7 @@ vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost" }, {
   pattern = "*",
   command = "silent! wall",
 })
+
+local filepathname = vim.fn.fnamemodify(vim.fn.getcwd(), ":~")
+vim.opt.titlestring = "nvim - " .. filepathname:gsub("^~/code/", "")
+vim.opt.title = true
