4cf9668d421a607fe8c23f5e7ced1550175b416c
Logan <logan-porter@outlook.com>
2026-06-25 22:13:25 +1200

    add spacing around hunk previews

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,6 +1,15 @@
 return {
   "lewis6991/gitsigns.nvim",
   config = function()
-    require("gitsigns").setup({ current_line_blame = true })
+    require("gitsigns").setup({
+      current_line_blame = true,
+      preview_config = {
+        border = { " ", " ", " ", " ", " ", " ", " ", " " },
+        style = "minimal",
+        relative = "cursor",
+        row = 0,
+        col = 1,
+      },
+    })
   end
 }
