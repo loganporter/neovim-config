7287ef8d12b5e985e5e88a55bd01443ed20a498c
Logan <logan-porter@outlook.com>
2026-06-25 21:52:22 +1200

    switch to normal mode in terminals after exiting lazygit

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -11,4 +11,15 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
   },
+  init = function()
+    -- When lazygit exits, if it was launched from a terminal buffer, return
+    -- that terminal to normal mode instead of dropping back into terminal mode.
+    vim.g.lazygit_on_exit_callback = function()
+      if vim.bo.buftype == "terminal" then
+        vim.schedule(function()
+          vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-\\><C-n>", true, false, true), "n", false)
+        end)
+      end
+    end
+  end,
 }
