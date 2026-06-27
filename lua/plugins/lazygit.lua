c09c006f1d89b82ebb404cbb5bf1759b58040a38
Logan <logan-porter@outlook.com>
2026-06-27 16:21:13 +1200

    use bundled config for lazygit when in nvim

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -12,6 +12,11 @@ return {
     "nvim-lua/plenary.nvim",
   },
   init = function()
+    -- Use the lazygit config bundled with this nvim repo so keybinding overrides
+    -- (e.g. <esc> to quit) work on any device without per-machine setup.
+    vim.g.lazygit_use_custom_config_file_path = 1
+    vim.g.lazygit_config_file_path = vim.fn.stdpath("config") .. "/lazygit/config.yml"
+
     -- When lazygit exits, if it was launched from a terminal buffer, return
     -- that terminal to normal mode instead of dropping back into terminal mode.
     vim.g.lazygit_on_exit_callback = function()
