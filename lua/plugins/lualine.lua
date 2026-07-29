d89366bdc95dc7399ed2e20f00ef7faa16449624
Logan <logan-porter@outlook.com>
2026-07-29 22:30:34 +1200

    add relative paths in lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -12,7 +12,31 @@ return {
         },
       },
       sections = {
+        lualine_c = {
+          {
+            "filename",
+            path = 1, -- 0=name, 1=relative, 2=absolute, 3=absolute+~, 4=name+parent
+            symbols = {
+              modified = "[+]",
+              readonly = "[-]",
+              unnamed = "[No Name]",
+            },
+          },
+        },
         lualine_x = { codecompanion_lualine, "diagnostics", "encoding", "fileformat", "filetype" }
+      },
+      inactive_sections = {
+        lualine_c = {
+          {
+            "filename",
+            path = 1, -- 0=name, 1=relative, 2=absolute, 3=absolute+~, 4=name+parent
+            symbols = {
+              modified = "[+]",
+              readonly = "[-]",
+              unnamed = "[No Name]",
+            },
+          },
+        },
       }
     })
   end
